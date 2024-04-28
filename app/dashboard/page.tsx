c656f0ac6eca695667e061a27b10ca8f23d4a8bb
superhan-dev <superhan.dev@gmail.com>
2024-04-28 17:53:23 +1000

    needle gauge chart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import BarChart from "@/components/BarChart";
 import Header from "@/components/Header";
 import LineGuage from "@/components/LineGuage";
+import NeedleGauge from "@/components/NeedleGauge/NeedleGauge";
 import RecentOrders from "@/components/RecentOrders";
 import SideNavbar from "@/components/SideNavbar";
 import TopCards from "@/components/TopCards";
@@ -8,7 +9,7 @@ import React from "react";
 
 const DashboardPage = () => {
   return (
-    <main className="flex min-h-screen bg-gray-100">
+    <main className="flex h-screen bg-gray-100">
       <SideNavbar />
       <div className="flex-col w-screen h-screen">
         <Header />
@@ -16,6 +17,7 @@ const DashboardPage = () => {
         <div className="p-4 grid md-grid-cols-3 grid-cols-1 gap-4">
           <BarChart />
           <LineGuage />
+          <NeedleGauge />
           <RecentOrders />
         </div>
       </div>
---
+++ components/NeedleGauge/NeedleGauge.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import * as d3 from "d3";
+
+const NeedleGauge = () => {
+  const ref = useRef<any>(null);
+  var width = 1000;
+  var height = 600;
+
+  var scale = d3.scaleLinear().domain([50, 80]).range([0, 180]);
+
+  useEffect(() => {
+    var svg = d3
+      .select(ref.current)
+      .attr("width", width)
+      .attr("height", height);
+
+    var arc: any = d3
+      .arc()
+      .innerRadius(50)
+      .outerRadius(80)
+      // .cornerRadius(5)
+      .padAngle(0.05);
+
+    // an array of colors
+    const colors: any[] = ["#ea4335", "#fbbc05", "#4285f4", "#34a853"];
+
+    var pie = d3
+      .pie()
+      .startAngle((-1 * Math.PI) / 2)
+      .endAngle(Math.PI / 2)
+      .value(function (colors: any) {
+        return 30 / colors.length;
+      });
+
+    // draw the arcs. one for each color
+    svg
+      .selectAll(".arc")
+      .data(pie(colors))
+      .enter()
+      .append("path")
+      .attr("d", arc)
+      .attr("transform", "translate(200,200)")
+      .style("fill", function (d, i) {
+        return colors[i];
+      });
+
+    // set up the needle
+    svg
+      .selectAll(".needle")
+      .data([0])
+      .enter()
+      .append("line")
+      .attr("x1", 0)
+      .attr("x2", -78)
+      .attr("y1", 0)
+      .attr("y2", 0)
+      .classed("needle", true)
+      .style("stroke", "black")
+      .style("stroke-width", "3")
+      .attr("transform", function (d) {
+        return " translate(200,200) rotate(" + d + ")";
+      });
+  }, []);
+
+  useEffect(() => {
+    var svg = d3.select(ref.current);
+    svg
+      .selectAll(".needle")
+      .data([50.5]) // 50.5(1) ~ 79.5(100)
+      // .transition()
+      // // .ease(d3.easeElasticOut)
+      // .duration(1000)
+      .attr("transform", function (d) {
+        return "translate(200,200) rotate(" + scale(d) + ")";
+      });
+  }, []);
+
+  return <svg ref={ref}>NeedleGauge</svg>;
+};
+
+export default NeedleGauge;
